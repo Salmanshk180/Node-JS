9645839c011a8e16ce7adc127cf489cc53424348
Salman Shaikh <salmanshaikh7118@gmail.com>
2024-03-11 15:59:14 +0530

    Express tutorial

---
+++ 07-Express-Intro/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+const app = express();
+app.get("/", (req: Request, res: Response) => {
+  res.send("Home Page");
+});
+
+app.get("/about", (req: Request, res: Response) => {
+  res.send("About Page");
+});
+
+app.listen(8000, () => console.log("Server listening on port:8000"));
