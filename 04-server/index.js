f34727a6d657359b98b1461779f4cd53c0262b1d
Salman Shaikh <salmanshaikh7118@gmail.com>
2024-03-11 14:19:07 +0530

    add gitignore

--- 04-server/index.js
+++ 04-server/index.js
@@ -1,13 +1,15 @@
 "use strict";
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
     const log = `${Date.now()} new request\n`;
-    fs.appendFile("log.txt", log, (err, result) => {
+    fs.appendFile("log.txt", log, (err) => {
         if (err) {
             console.log(err);
             return;
         }
     });
+    console.log(req);
+    console.log(res);
 });
-server.listen(8000, () => console.log('server listening on port 8000'));
+server.listen(8000, () => console.log("server listening on port 8000"));
--- 04-server/index.ts
+++ 04-server/index.ts
@@ -2,18 +2,20 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer(
-  (req: NodeJS.ErrnoException, res: NodeJS.ErrnoException) => {
+  (req:Request, res:Response) => {
     const log = `${Date.now()} new request\n`;
     fs.appendFile(
       "log.txt",
       log,
-      (err: NodeJS.ErrnoException, result: Buffer) => {
+      (err: NodeJS.ErrnoException | null) => {
         if (err) {
           console.log(err);
           return;
         }
       }
     );
+    console.log(req);
+    console.log(res);
   }
 );
 
